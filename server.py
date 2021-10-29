a3685f66e039bb818cbf7fadf1139f48033cac2b
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-28 22:19:29 -0600

    Functiong X,Y

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-        return render_template('index.html')
+        return render_template('index.html', row=int(10), col=int(10))
+
+@app.route('/4')
+def cb4x8():
+        return render_template('index.html', row=int(4), col=int(8))
+
+@app.route('/<int:x>/<int:y>')
+def cbxy(x, y):
+        return render_template('index.html', row=x, col=y)
 
 if __name__ ==  "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
 
